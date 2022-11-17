8d5f20e47e45787ad71297bd16ccd3dac0706869
ryanzin <ryangordi3@gmail.com>
2022-11-17 11:20:22 -0300

    Update BUG-MAP.lua

--- BUG-MAP.lua
+++ BUG-MAP.lua
@@ -5,14 +5,42 @@ bugMap.useDiagonals = true
 bugMap.isKeyPressed = modules.corelib.g_keyboard.isKeyPressed
 
 bugMap.directions = {
-    ["W"] = {0, -5},
-    ["E"] = {3, -3},
-    ["D"] = {5, 0},
-    ["C"] = {3, 3},
-    ["S"] = {0, 5},
-    ["Z"] = {-3, 3},
-    ["A"] = {-5, 0},
-    ["Q"] = {-3, 3}
+    ["W"] = {
+        0,
+        -5,
+        0
+    },
+    ["E"] = {
+        3,
+        -3
+    },
+    ["D"] = {
+        5,
+        0,
+        1
+    },
+    ["C"] = {
+        3,
+        3
+    },
+    ["S"] = {
+        0,
+        5,
+        2
+    },
+    ["Z"] = {
+        -3,
+        3
+    },
+    ["A"] = {
+        -5, 
+        0,
+        3
+    },
+    ["Q"] = {
+        -3
+        3
+    }
 }
 
 bugMap.macro = macro(
@@ -22,7 +50,12 @@ bugMap.macro = macro(
         local pos = pos()
         for key, dir in pairs(bugMap.directions) do
             if bugMap.isKeyPressed(key) then
-                if dir[1] == 0 or dir[2] == 0 or bugMap.useDiagonals then
+                if dir[3] then
+                    if player:getDirection() ~= dir[3] then
+                        turn(dir[3])
+                    end
+                end
+                if bugMap.useDiagonals or dir[1] == 0 or dir[2] == 0 then
                     local tile = g_map.getTile({x = pos.x + dir[1], y = pos.y + dir[2], z = pos.z})
                     if tile then
                         return g_game.use(tile:getTopUseThing())
