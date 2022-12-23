06fa83a49880462af2f67a641840dc713527def8
ryanzin <ryangordi3@gmail.com>
2022-12-23 11:01:20 -0300

    Create xNameSense.lua

---
+++ xNameSense.lua
@@ -0,0 +1,23 @@
+macro(1, 'xSense', 'F12', function()
+    if storage.Sense then
+        locatePlayer = getPlayerByName(storage.Sense)
+        if not (locatePlayer and locatePlayer:getPosition().z == player:getPosition().z and getDistanceBetween(pos(), locatePlayer:getPosition()) <= 6) then
+            say('sense "' .. storage.Sense)
+            delay(1000)
+        end
+    end
+end)
+
+
+onTalk(function(name, level, mode, text, channelId, pos)
+    if player:getName() == name then
+        if string.sub(text, 1, 1) == 'x' then
+            local checkMsg = string.sub(text, 2):trim()
+            if checkMsg == '0' then
+                storage.Sense = false
+            else
+                storage.Sense = checkMsg
+            end
+        end
+    end
+end)
