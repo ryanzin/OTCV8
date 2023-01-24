19906c00925247863e77f33e76e9e46f3539f143
ryanzin <ryangordi3@gmail.com>
2023-01-24 09:57:59 -0300

    Update ANTI-RED.lua

--- ANTI-RED.lua
+++ ANTI-RED.lua
@@ -11,37 +11,27 @@ addTextEdit("Area", storage.areaSpell or "Magia de Area", function(widget, text)
 end)
 
 local timeArea = 0
-macro(
-    1,
-    "Anti-Red",
-    function()
-        local pos = pos()
-        local monstersCount = 0
-        for _, spec in pairs(getSpectators(true)) do
-            if monstersCount > 1 or timeArea > now then
-                break
-            end
-            local specPos = spec:getPosition()
-            local checkPosz = math.abs(specPos.z - pos.z)
-            if checkPosz <= 3 then
-                if
-                    (spec ~= player and spec:isPlayer() and spec:getEmblem() ~= 1 and spec:getShield() < 3) or player:getSkull() >= 3
-                 then
-                    timeArea = now + 30000
-                    break
-                elseif checkPosz == 0 and spec:isMonster() and getDistanceBetween(specPos, pos) == 1 then
-                    monstersCount = monstersCount + 1
-                end
-            end
-        end
-        if monstersCount > 1 and (not timeArea or timeArea < now) then
-            return say(storage.areaSpell)
-        end
-        if not g_game.isAttacking() then
-            return
-        end
-        for _, spell in ipairs(storage.comboSpells:split(',')) do
-            say(spell)
-        end
-    end
-)
+macro(1, "Anti-Red", function()
+	local pos = pos()
+	local monstersCount = 0
+	for _, spec in pairs(getSpectators(true)) do
+		if monstersCount > 1 or timeArea > now then break end
+		local specPos = spec:getPosition()
+		local checkPosz = math.abs(specPos.z - pos.z)
+		if checkPosz <= 3 then
+			if (spec ~= player and spec:isPlayer() and spec:getEmblem() ~= 1 and spec:getShield() < 3) or player:getSkull() >= 3 then
+				timeArea = now + 30000
+				break
+			elseif checkPosz == 0 and spec:isMonster() and getDistanceBetween(specPos, pos) == 1 then
+				monstersCount = monstersCount + 1
+			end
+		end
+	end
+	if monstersCount > 1 and (not timeArea or timeArea < now) then
+		return say(storage.areaSpell)
+	end
+	if not g_game.isAttacking() then return end
+   	for _, spell in ipairs(storage.comboSpells:split(',')) do
+		say(spell)
+	end
+end)
