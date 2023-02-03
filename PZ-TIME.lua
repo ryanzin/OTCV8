11d7c5195d77fcd741512c0a6975fdddd8ba5af9
ryanzin <ryangordi3@gmail.com>
2023-02-02 21:55:45 -0300

    Update PZ-TIME.lua

--- PZ-TIME.lua
+++ PZ-TIME.lua
@@ -125,6 +125,7 @@ end
 pkTimeMacro = macro(1, function()
 	local time = os and os.time() or now
 	if (os and battleLastVerified ~= time) or (not os and (not battleLastVerified or battleLastVerified < time)) then
+		if isInPz() then storage.battleTracking[1] = 0 end
 		for specName, value in pairs(storage.battleTracking[3]) do
 			if (os and value.timeBattle >= time) or (not os and value.timeBattle >= time and value.timeBattle - 60000 <= time) then
 				local playerSearch = getPlayerByName(specName, true)
