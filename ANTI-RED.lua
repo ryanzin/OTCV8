2b89d6a1286f1d4842191cca1539fa356ebb4081
ryanzin <ryangordi3@gmail.com>
2023-04-11 21:18:13 -0300

    Update ANTI-RED.lua

--- ANTI-RED.lua
+++ ANTI-RED.lua
@@ -1,58 +1,55 @@
-setDefaultTab('Tools')
-
-
 UI.Label('Combo:')
 addTextEdit("Magias", storage.comboSpells or "magia1, magia2, magia3", function(widget, text)
-	storage.comboSpells = text
+	storage.comboSpells = text;
 end)
 
 
 UI.Label('Area:')
 addTextEdit("Area", storage.areaSpell or "Magia de Area", function(widget, text)
-	storage.areaSpell = text
+	storage.areaSpell = text;
 end)
 
 if g_game.getWorldName() == 'Katon' then -- FIX NTO SPLIT
 	function getSpecs()
-		local specs = {}
-		local tiles = g_map.getTiles(posz())
+		local specs = {};
+		local tiles = g_map.getTiles(posz());
 		for i = 1, #tiles do
-			local tile = tiles[i]
+			local tile = tiles[i];
 			for _, thing in pairs(tile:getThings()) do
-				if thing:isCreature() then
-					table.insert(specs, thing)
+				if (thing:isCreature()) then
+					table.insert(specs, thing);
 				end
 			end
 		end
-		return specs
+		return specs;
 	end
 end
 
-local timeArea = 0
+local timeArea = 0;
 macro(1, "Anti-Red", function()
-	local pos = pos()
-	local monstersCount = 0
-	timeArea = player:getSkull() >= 3 and now + 30000 or timeArea
-	local specs = getSpecs and getSpecs() or getSpectators(true)
+	local pos = pos();
+	local monstersCount = 0;
+	timeArea = player:getSkull() >= 3 and now + 30000 or timeArea;
+	local specs = getSpecs and getSpecs() or getSpectators(true);
 	for i = 1, #specs do
-		local spec = specs[i]
-		if timeArea > now then break end
-		local specPos = spec:getPosition()
-		local checkPosz = math.abs(specPos.z - pos.z)
+		local spec = specs[i];
+		if (timeArea > now) then break; end
+		local specPos = spec:getPosition();
+		local checkPosz = math.abs(specPos.z - pos.z);
 		if checkPosz <= 3 then
 			if (spec ~= player and spec:isPlayer() and spec:getEmblem() ~= 1 and spec:getShield() < 3) then
-				timeArea = now + 30000
+				timeArea = now + 30000;
 				break
 			elseif checkPosz == 0 and spec:isMonster() and getDistanceBetween(specPos, pos) == 1 then
-				monstersCount = monstersCount + 1
+				monstersCount = monstersCount + 1;
 			end
 		end
 	end
 	if monstersCount > 1 and (not timeArea or timeArea < now) then
-		return say(storage.areaSpell)
+		return say(storage.areaSpell);
 	end
-	if not g_game.isAttacking() then return end
+	if (not g_game.isAttacking()) then return; end
    	for _, spell in ipairs(storage.comboSpells:split(',')) do
-		say(spell)
+		say(spell);
 	end
 end)
