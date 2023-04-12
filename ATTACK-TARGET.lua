d3451d8eb3f908a44868593b07c3ecf65d541bdb
ryanzin <ryangordi3@gmail.com>
2023-04-11 21:13:53 -0300

    Update ATTACK-TARGET.lua

--- ATTACK-TARGET.lua
+++ ATTACK-TARGET.lua
@@ -1,35 +1,26 @@
-setDefaultTab('Tools')
-
 local keepTarget = {}
 
 keepTarget.keyCancel = "Escape"
 
-keepTarget.macro =
-    macro(
-    1,
-    "Attack target",
-    function()
-        if modules.corelib.g_keyboard.isKeyPressed(keepTarget.keyCancel) then
-            keepTarget.storageId = nil
-            return g_game.cancelAttack()
+keepTarget.macro = macro(1, "Attack target", function()
+	if modules.corelib.g_keyboard.isKeyPressed(keepTarget.keyCancel) then
+		keepTarget.storageId = nil;
+		return g_game.cancelAttack();
+	end
+	local target = g_game.getAttackingCreature();
+	if target then
+		local targetId = target:getId();
+		if keepTarget.storageId ~= targetId then
+			keepTarget.storageId = targetId;
+		end
+		return;
+	else
+		if keepTarget.storageId then
+			local findCreature = getCreatureById(keepTarget.storageId);
+			if findCreature then
+				g_game.attack(findCreature);
+			end
+			return delay(findCreature and 500 or 100);
         end
-        local target = g_game.getAttackingCreature()
-        if target then
-            local targetId = target:getId()
-            if keepTarget.storageId ~= targetId then
-                keepTarget.storageId = targetId
-            end
-            return
-        else
-            if keepTarget.storageId then
-                local findCreature = getCreatureById(keepTarget.storageId)
-                if findCreature then
-                    g_game.attack(findCreature)
-                    return delay(500)
-                else
-                    return delay(100)
-                end
-            end
-        end
-    end
-)
+	end
+end, mainTab)
