f0a34139937b6357a8ee86adf424950c4ffd6251
ryanzin <ryangordi3@gmail.com>
2023-05-04 17:15:31 -0300

    Update BUG-MAP.lua

--- BUG-MAP.lua
+++ BUG-MAP.lua
@@ -1,21 +1,25 @@
-local bugMap = {}
+panel = mainTab;
+
+local bugMap = {};
 
 bugMap.checkBox = setupUI([[
 CheckBox
   id: checkBox
   font: cipsoftFont
   text: Use Diagonal
-]])
+]]);
 
 bugMap.checkBox.onCheckChange = function(widget, checked)
-    storage.bugMapCheck = checked
+    storage.bugMapCheck = checked;
 end
 
-if storage.bugMapCheck == nil then storage.bugMapCheck = true end
+if storage.bugMapCheck == nil then
+	storage.bugMapCheck = true;
+end
 
-bugMap.checkBox:setChecked(storage.bugMapCheck)
+bugMap.checkBox:setChecked(storage.bugMapCheck);
 
-bugMap.isKeyPressed = modules.corelib.g_keyboard.isKeyPressed
+bugMap.isKeyPressed = modules.corelib.g_keyboard.isKeyPressed;
 
 bugMap.directions = {
     ["W"] = {x = 0, y = -5, direction = 0},
@@ -26,18 +30,22 @@ bugMap.directions = {
     ["Z"] = {x = -3, y = 3},
     ["A"] = {x = -5, y = 0, direction = 3},
     ["Q"] = {x = -3, y = 3}
-}
+};
 
 bugMap.macro = macro(1, "Bug Map", function()
-    if modules.game_console:isChatEnabled() or modules.corelib.g_keyboard.isCtrlPressed() then return end
-    local pos = pos()
+    if (modules.game_console:isChatEnabled() or modules.corelib.g_keyboard.isCtrlPressed()) then return; end
+    local pos = pos();
     for key, config in pairs(bugMap.directions) do
-        if bugMap.isKeyPressed(key) then
-            if storage.bugMapCheck or config.direction then
-                if config.direction then turn(config.direction) end
-                local tile = g_map.getTile({x = pos.x + config.x, y = pos.y + config.y, z = pos.z})
-                if tile then return g_game.use(tile:getTopUseThing()) end
+        if (bugMap.isKeyPressed(key)) then
+            if (storage.bugMapCheck or config.direction) then
+                if (config.direction) then
+					turn(config.direction);
+				end
+                local tile = g_map.getTile({x = pos.x + config.x, y = pos.y + config.y, z = pos.z});
+                if (tile) then
+					return g_game.use(tile:getTopUseThing());
+				end
             end
         end
     end
-end, mainTab)
+end)
