d82b45c7d7fe83d5e48fdbec57e12ece9c284bed
ryanzin <ryangordi3@gmail.com>
2023-05-27 17:55:56 -0300

    Update ANTI-RED.lua

--- ANTI-RED.lua
+++ ANTI-RED.lua
@@ -1,5 +1,39 @@
 UI.Label('Combo:')
 
+storage.widgetPos = storage.widgetPos or {};
+
+local antiRedTimeWidget = setupUI([[
+UIWidget
+  background-color: black
+  opacity: 0.8
+  padding: 0 5
+  focusable: true
+  phantom: false
+  draggable: true
+]], g_ui.getRootWidget())
+
+
+antiRedTimeWidget.onDragEnter = function(widget, mousePos)
+	if not (modules.corelib.g_keyboard.isAltPressed()) then return; end
+	widget:breakAnchors();
+	local widgetPos = widget:getPosition();
+	widget.movingReference = {x = mousePos.x - widgetPos.x, y = mousePos.y - widgetPos.y};
+	return true;
+end
+
+antiRedTimeWidget.onDragMove = function(widget, mousePos, moved)
+	local parentRect = widget:getParent():getRect();
+	local x = math.min(math.max(parentRect.x, mousePos.x - widget.movingReference.x), parentRect.x + parentRect.width - widget:getWidth());
+	local y = math.min(math.max(parentRect.y - widget:getParent():getMarginTop(), mousePos.y - widget.movingReference.y), parentRect.y + parentRect.height - widget:getHeight());   
+	widget:move(x, y);
+	storage.widgetPos.antiRedTime = {x = x, y = y};
+	return true
+end
+
+local name = "antiRedTime";
+storage.widgetPos[name] = storage.widgetPos[name] or {};
+antiRedTimeWidget:setPosition({x = storage.widgetPos[name].x or 50, y = storage.widgetPos[name].y or 50});
+
 
 
 local refreshSpells = function()
@@ -41,17 +75,41 @@ if (not getSpectators or #getSpectators(true) == 0) then
 	end
 end
 
-local timeArea = 0;
+if (not storage.antiRedTime or storage.antiRedTime - 30000 > now) then
+	storage.antiRedTime = 0;
+end
+
+
+local addAntiRedTime = function()
+	storage.antiRedTime = now + 30000;
+end
+
+local toInteger = function(t)
+	t = tostring(t);
+	t = t:split(".");
+	return tonumber(t[1]);
+end
+
 macro(1, "Anti-Red", function()
 	local pos, monstersCount = pos(), 0;
-	timeArea = player:getSkull() >= 3 and now + 30000 or timeArea;
+	if (storage.antiRedTime >= now) then
+		antiRedTimeWidget:show();
+		local diff = storage.antiRedTime - now;
+		diff = diff / 1000;
+		antiRedTimeWidget:setText(tr("AREA BLOCKED FOR %d seconds.", toInteger(diff)));
+	else
+		antiRedTimeWidget:hide();
+	end
+	if (player:getSkull() >= 3) then
+		addAntiRedTime();
+	end
 	local specs = getSpectators(true);
 	for _, spec in ipairs(specs) do
 		local specPos = spec:getPosition();
 		local floorDiff = math.abs(specPos.z - pos.z);
 		if (floorDiff > 3) then goto continue; end
 		if (spec ~= player and spec:isPlayer() and spec:getEmblem() ~= 1 and spec:getShield() < 3) then
-			timeArea = now + 30000;
+			addAntiRedTime();
 			break
 		elseif (floorDiff == 0 and spec:isMonster() and getDistanceBetween(specPos, pos) == 1) then
 			monstersCount = monstersCount + 1;
